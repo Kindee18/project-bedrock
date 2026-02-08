6ebcaa903154ca2564623a75f0ec4099cab60e92
Kindson <kindson@project-bedrock.local>
2026-02-08 11:26:37 +0100

    Fix CI/CD: Configure Helm/K8s providers with EKS auth

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -27,5 +27,24 @@ provider "aws" {
   }
 }
 
-# The kubernetes and helm providers will be configured after the EKS cluster is created.
-# This usually requires a data source for the cluster credentials.
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_name
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_name
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
+  }
+}
